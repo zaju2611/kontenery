cfa853ed86ba7fe0c795852f74707db131617878
Jakub Zając <jakub.zajac2611@gmail.com>
2024-12-29 18:11:17 +0100

    add color metal page

--- slider.js
+++ slider.js
@@ -1,30 +1,27 @@
-let currentIndex = 0; // Indeks bieżącego obrazu
-const sliderBox = document.querySelector(".slider-box"); // Kontener dla obrazków
-const images = document.querySelectorAll(".slider-img"); // Wszystkie obrazki w sliderze
-const totalImages = images.length; // Łączna liczba obrazów
+let currentIndex = 0;
+const sliderBox = document.querySelector(".slider-box");
+const images = document.querySelectorAll(".slider-img");
+const totalImages = images.length;
 
-// Funkcja do aktualizacji pozycji slidera
 function updateSliderPosition() {
-	const offset = -currentIndex * 100; // Przesunięcie o 100% dla każdego obrazu
-	sliderBox.style.transform = `translateX(${offset}%)`; // Ustawienie przemieszczenia
+	const offset = -currentIndex * 100;
+	sliderBox.style.transform = `translateX(${offset}%)`;
 }
 
-// Funkcja do przechodzenia do poprzedniego obrazu
 document.querySelector(".btn-left").addEventListener("click", () => {
 	if (currentIndex > 0) {
 		currentIndex--;
 	} else {
-		currentIndex = totalImages - 1; // Jeśli na początku, przejdź na ostatni obraz
+		currentIndex = totalImages - 1;
 	}
 	updateSliderPosition();
 });
 
-// Funkcja do przechodzenia do następnego obrazu
 document.querySelector(".btn-right").addEventListener("click", () => {
 	if (currentIndex < totalImages - 1) {
 		currentIndex++;
 	} else {
-		currentIndex = 0; // Jeśli na końcu, przejdź na pierwszy obraz
+		currentIndex = 0;
 	}
 	updateSliderPosition();
 });
