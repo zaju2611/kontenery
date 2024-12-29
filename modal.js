bd16cd3c58ac65e373a703a36953fd60b30d1b9f
Jakub Zając <jakub.zajac2611@gmail.com>
2024-12-29 13:04:13 +0100

    added next subpages

--- modal.js
+++ modal.js
@@ -5,53 +5,59 @@ function openModal(imgSrc) {
 	modalImg.src = imgSrc;
 }
 
-// Funkcja do zamykania modalu
 function closeModal() {
 	const modal = document.getElementById("modal");
 	modal.style.display = "none";
 }
 
-// Funkcja do przewijania zdjęć
 let currentImageIndex = 0;
-const images = document.querySelectorAll(".grid-wrapper img");
+let currentSection = null;
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
 
 function showImage(index) {
-	const modalImg = document.getElementById("modal-img");
-	modalImg.src = images[index].src;
-	currentImageIndex = index;
+	const images = currentSection.querySelectorAll("img");
+	modalImg.style.opacity = 0;
+
+	setTimeout(() => {
+		modalImg.src = images[index].src;
+		modalImg.style.opacity = 1;
+		currentImageIndex = index;
+	}, 500);
 }
 
-// Obsługuje kliknięcia na zdjęcia
-document.querySelectorAll(".grid-wrapper img").forEach((img, index) => {
-	img.addEventListener("click", () => {
-		openModal(img.src);
-		currentImageIndex = index; // Ustawienie bieżącego indeksu
+document.querySelectorAll("section").forEach((section) => {
+	const images = section.querySelectorAll(".grid-wrapper img");
+
+	images.forEach((img, index) => {
+		img.addEventListener("click", () => {
+			openModal(img.src);
+			currentSection = section;
+			currentImageIndex = index;
+		});
 	});
 });
 
-// Obsługuje kliknięcie w przycisk zamykania
 document.getElementById("close-btn").addEventListener("click", closeModal);
 
-// Obsługuje kliknięcie w przyciski nawigacyjne
 document.getElementById("prev-btn").addEventListener("click", () => {
 	if (currentImageIndex > 0) {
 		showImage(currentImageIndex - 1);
 	} else {
-		showImage(images.length - 1);
+		showImage(currentSection.querySelectorAll("img").length - 1);
 	}
 });
 
 document.getElementById("next-btn").addEventListener("click", () => {
-	if (currentImageIndex < images.length - 1) {
+	if (currentImageIndex < currentSection.querySelectorAll("img").length - 1) {
 		showImage(currentImageIndex + 1);
 	} else {
 		showImage(0);
 	}
 });
 
-// Zamykanie modalu po kliknięciu poza obrazem
 document.getElementById("modal").addEventListener("click", (e) => {
-	if (e.target === document.getElementById("modal")) {
+	if (e.target === modal) {
 		closeModal();
 	}
 });
