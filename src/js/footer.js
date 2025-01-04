af084837f796d65ef4f99b33e3b18f534e3da482
Jakub Zając <jakub.zajac2611@gmail.com>
2025-01-04 21:21:59 +0100

    added new function to footer, <strong> to texts on pages, fixed some bugs

--- src/js/footer.js
+++ src/js/footer.js
@@ -6,3 +6,17 @@ const handleCurrentYear = () => {
 };
 
 handleCurrentYear();
+
+const addActiveClassToFooterLink = () => {
+	const currentPath = window.location.pathname; // Pobiera ścieżkę URL bez domeny i portu
+	const footerLinks = document.querySelectorAll(".footer-nav a"); // Wszystkie linki w stopce
+
+	footerLinks.forEach((link) => {
+		const linkPath = new URL(link.href).pathname; // Pobiera ścieżkę z atrybutu href linku
+		if (linkPath === currentPath) {
+			link.classList.add("activeFooter");
+		}
+	});
+};
+
+addActiveClassToFooterLink();
