db1135e52c2334f26d9169af8ab05bf5f6dbfd70
Jakub Zając <jakub.zajac2611@gmail.com>
2025-01-19 20:29:44 +0100

    changed photo look

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,7 +1,28 @@
 const buttons = document.querySelectorAll("[data-carousel-button]");
+let intervalId;
+
+function startCarousel() {
+	intervalId = setInterval(() => {
+		const carousel = document.querySelector("[data-carousel]");
+		const slides = carousel.querySelector("[data-slides]");
+		const activeSlide = slides.querySelector("[data-active]");
+		const offset = 1;
+
+		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+		if (newIndex >= slides.children.length) newIndex = 0;
+
+		slides.children[newIndex].dataset.active = true;
+		delete activeSlide.dataset.active;
+	}, 7000);
+}
+
+startCarousel();
 
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
+		clearInterval(intervalId);
+		startCarousel();
+
 		const offset = button.dataset.carouselButton === "next" ? 1 : -1;
 		const slides = button
 			.closest("[data-carousel]")
