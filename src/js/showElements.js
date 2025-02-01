728977ffd89bde6ef94fb1a2d748a6377e1f2e1e
Jakub Zając <jakub.zajac2611@gmail.com>
2025-02-01 16:13:51 +0100

    add hidden elements on pages

---
+++ src/js/showElements.js
@@ -0,0 +1,14 @@
+const elements = document.querySelectorAll(".hidden-element");
+
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("show");
+			}
+		});
+	},
+	{ threshold: 0.2 }
+);3
+
+elements.forEach((el) => observer.observe(el));
